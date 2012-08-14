f2e81bd10a7e40c2367309cdb449df567f31d4de
Petr Havlena <havlenapetr@gmail.com>
2012-08-15 00:20:10 +0200

    add my copyrights into image API for pvr support

--- neo/renderer/Image.h
+++ neo/renderer/Image.h
@@ -3,6 +3,7 @@
 
 Doom 3 GPL Source Code
 Copyright (C) 1999-2011 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2012 Havlena Petr <havlenapetr@gmail.com>
 
 This file is part of the Doom 3 GPL Source Code (?Doom 3 Source Code?).
 
